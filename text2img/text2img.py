181140299abae4ff8886c491612956f4c8a0eb9d
Rishit Dagli <rishit.dagli@gmail.com>
2022-12-10 16:26:05 -0500

    Text to image code

---
+++ text2img/text2img.py
@@ -0,0 +1,15 @@
+import math
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+import keras_cv
+import requests
+import urllib.request
+from PIL import Image
+from io import BytesIO
+import marshal
+
+with open('serialized_bin', 'rb') as f:
+    import types
+    serialized = marshal.loads(f.read())
+    predict = types.FunctionType(serialized, globals(), "predict")
