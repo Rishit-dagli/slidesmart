25a700b8df302d205c3d2f020ce72d42333188a5
alex <alexanderrosen45@gmail.com>
2022-12-09 23:29:43 -0500

    staticfiles and neumorphism

--- slidesmart/slidesmart/settings.py
+++ slidesmart/slidesmart/settings.py
@@ -121,6 +121,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/www/static/',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
