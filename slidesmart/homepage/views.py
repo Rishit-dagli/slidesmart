14b8b07882f91a8045def7daae1c7731bdd03a2e
alex <alexanderrosen45@gmail.com>
2022-12-10 12:13:42 -0500

    upload text files

--- slidesmart/homepage/views.py
+++ slidesmart/homepage/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+
+def simple_upload(request):
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        return render(request, 'core/simple_upload.html', {
+            'uploaded_file_url': uploaded_file_url
+        })
+    return render(request, 'core/simple_upload.html')
 
 def index(request):
     context = {}
+
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        context.update({'uploaded_file_url': uploaded_file_url})
+    print(str(context))
 
     return render(request, 'index.html', context=context)
